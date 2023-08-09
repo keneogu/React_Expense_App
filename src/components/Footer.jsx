1c0880ec726b896b064a4225bbc1a06de7678d49
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-08-09 23:51:03 +0100

    Create footer page component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<div className='w-full h-10 bg-slate-900 text-slate-400 relative bottom-0 z-10'>
+			<p>This is the footer</p>
+		</div>
+	)
+}
+
+export default Footer;
