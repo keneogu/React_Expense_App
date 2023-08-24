d99239570cd82c6bdd47550a2b22f977ed90f8b5
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-08-24 22:46:03 +0100

    Add LoginUser reducer and remove commented codes

--- src/reducers/authContext.jsx
+++ src/reducers/authContext.jsx
@@ -1,54 +1,62 @@
-import React, { createContext, useReducer } from 'react';
-import authReducer from './authReducer';
-import axios from 'axios';
+import React, { createContext, useReducer } from "react";
+import authReducer from "./authReducer";
+import axios from "axios";
 
 const initialState = {
   user: {},
   error: null,
   loading: false,
-	isAuthenticate: false
-}
+  isAuthenticate: true,
+};
 
-// Create context
 export const AuthContext = createContext(initialState);
 
-// Provider component
 export const AuthProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
-  // Actions
-  async function login(usernameEmail, password) {
+  async function loginUser(username, email, password) {
     try {
+      dispatch({ type: "LOGIN_REQUEST" });
 
-			dispatch({ type: 'LOGIN_REQUEST'})
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
 
-			const config = {
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}
+      const { data } = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        { username, email, password },
+        config
+      );
 
-      const {data} = await axios.post('http://localhost:5000/api/auth/login', {usernameEmail, password }, config);
-			console.log(data);
+      if (data) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+      }
+
       dispatch({
-        type: 'LOGIN_SUCCESS',
-        payload: data.user
+        type: "LOGIN_SUCCESS",
+        payload: data.user,
       });
     } catch (error) {
       dispatch({
-        type: 'LOGIN_FAIL',
-        payload: error.response.data.message
+        type: "LOGIN_FAIL",
+        payload: error.response.data.message,
       });
     }
   }
 
-	return (<AuthContext.Provider value={{
-    user: state.user,
-    error: state.error,
-    loading: state.loading,
-		isAuthenticate: state.isAuthenticate,
-		login
-  }}>
-    {children}
-  </AuthContext.Provider>);
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        error: state.error,
+        loginUser,
+        loading: state.loading,
+        isAuthenticate: state.isAuthenticate,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
