97ae48951365fa90c5351efaa7f2c85761615b21
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-08-25 14:59:02 +0100

    Add register route

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import './App.css'
-import Home from './pages/Home'
-import Footer from './components/Footer'
-import Header from './components/Header'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Login from "./pages/Login";
+import { AuthProvider } from "./reducers/authContext";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
-    <div className=''>
+    <AuthProvider>
       <Router>
-      <Header />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/register" exact element={<Register />} />
+        </Routes>
+        <Footer />
       </Router>
-    </div>
-  )
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
