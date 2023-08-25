dcd0e4110af5b09864adf6318d22f40a558e7cd7
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-08-25 14:58:10 +0100

    Add logout reducer action and type

--- src/reducers/authReducer.jsx
+++ src/reducers/authReducer.jsx
@@ -1,24 +1,35 @@
 export default (state, action) => {
-  switch(action.type) {
-    case 'LOGIN_REQUEST':
+  switch (action.type) {
+    case "LOGIN_REQUEST":
       return {
         loading: true,
-				isAuthenticate: false
-      }
-		case 'LOGIN_SUCCESS':
-			return {
-					...state,
-					loading: false,
-					isAuthenticate: true,
-					user: action.payload
-			}
-	case 'LOGIN_FAIL':
-		return {
-				loading: false,
-				isAuthenticate: false,
-				user: null
-		}
+        isAuthenticate: false,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        isAuthenticate: true,
+        user: action.payload,
+      };
+    case "LOGIN_FAIL":
+      return {
+        loading: false,
+        isAuthenticate: false,
+        user: null,
+      };
+    case "LOGOUT_SUCCESS":
+      return {
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+      };
+    case "LOGOUT_FAIL":
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
-}
+};
