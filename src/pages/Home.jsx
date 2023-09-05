b417ee823e0c92afe9322a7aaf67ab5820267a24
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-09-05 23:50:35 +0100

    Check if the user is Authenticate before moving to the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useEffect, useContext} from 'react'
+import { AuthContext } from "../reducers/authContext";
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+	const { login, isAuthenticate, error, loading } = useContext(AuthContext);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!isAuthenticate) {
+			navigate("/login")
+		}
+	}, [isAuthenticate])
+	
 	return (
-		<div>Homepage</div>
+		<div>Home</div>
 	)
 }
 
